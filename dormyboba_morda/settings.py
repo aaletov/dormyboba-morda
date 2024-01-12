1bc19d4cce8551bf250568b14c7cfd848bc4fa4d
aapozd <aapozdnyakove@gmail.com>
2024-01-12 22:54:39 +0000

    Working serve invite page
    Signed-off-by: aapozd <aapozdnyakove@gmail.com>

--- dormyboba_morda/settings.py
+++ dormyboba_morda/settings.py
@@ -30,6 +30,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'invite.apps.InviteConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
